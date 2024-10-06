fa9c18d8277e229b2d14bf8bd78ea766439e3250
jvermilllion <joshua.vermillion@gmail.com>
2024-10-06 11:46:45 -0400

    sanitizer again

--- plugins/input-sanitizer.ts
+++ plugins/input-sanitizer.ts
@@ -1,24 +1,22 @@
-import { defineNuxtPlugin } from '#app'
-
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive('sanitize', {
-    mounted(el, binding) {
-      const sanitizeInput = (event) => {
-        const input = event.target;
-        let sanitized = input.value.replace(/[<>&]/g, '');
-        if (binding.value === 'alphanumeric') {
-          sanitized = sanitized.replace(/[^a-zA-Z0-9]/g, '');
-        }
-        if (input.value !== sanitized) {
-          input.value = sanitized;
-          input.dispatchEvent(new Event('input'));
-        }
-      };
-
-      el.addEventListener('input', sanitizeInput);
-    },
-    unmounted(el) {
-      el.removeEventListener('input', el.__sanitizeHandler);
-    }
+    nuxtApp.vueApp.directive('sanitize', {
+      mounted(el, binding) {
+        const sanitizeInput = (event) => {
+          const input = event.target;
+          let sanitized = input.value.replace(/[<>&]/g, '');
+          if (binding.value === 'alphanumeric') {
+            sanitized = sanitized.replace(/[^a-zA-Z0-9]/g, '');
+          }
+          if (input.value !== sanitized) {
+            input.value = sanitized;
+            input.dispatchEvent(new Event('input'));
+          }
+        };
+  
+        el.addEventListener('input', sanitizeInput);
+      },
+      unmounted(el) {
+        el.removeEventListener('input', el.__sanitizeHandler);
+      }
+    });
   });
-});
